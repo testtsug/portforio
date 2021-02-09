55f7b5298ea092cd4916a5c98069f76e2c32cd2f
testtsug <78212841+testtsug@users.noreply.github.com>
2021-02-09 18:27:22 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,85 +1,60 @@
-// JavaScript Document
-
-
-//ローディングアニメーション
+//オープニング画面
 $(function () {
-    var loading = $('.block2');
-
-    $(window).on('load', function () {
-        $('.block2').fadeOut();
-    })
-
     setTimeout(function () {
-        $('.block2').fadeOut();
-    }, 1500);
+        $('.start p').fadeIn(1600);
+    }, 700); //0.7秒後にロゴをフェードイン!
+    setTimeout(function () {
+        $('.start').fadeOut(500);
+    }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
 
-//$(function () {
-//	var loader = $('.loader-wrap');
-//
-//	$(window).on('load', function () {
-//		$('.loader-wrap').fadeOut();
-//	});
-//
-//	setTimeout(function () {
-//		$('.loader-wrap').fadeOut();
-//	}, 1000);
-//});
 
-//ページ遷移時アニメーション
-//$(window).on('load', function () {
-//	$('body').removeClass('is-slide');
-//});
-//
-//$(function () {
-//
-//	$('a:not([href^="#"]):not([target])').on('click', function (e) {
-//		e.preventDefault();
-//		url = $(this).attr('href');
-//
-//		if (url !== '') {
-//			$('body').addClass('is-slide-in');
-//
-//			setTimeout(function () {
-//				window.location = url;
-//			}, 700);
-//		}
-//		return false;
-//	});
-//
-//});
 
-//トップへ戻る
+
 $(function () {
-    let toTop = $('.pagetop');
-    toTop.hide();
+    var loader = $('.loader-wrap');
 
+    //ページの読み込みが完了したらアニメーションを非表示
+    $(window).on('load', function () {
+        loader.fadeOut();
+    });
+
+    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+    setTimeout(function () {
+        loader.fadeOut();
+    }, 3000);
+});
+
+
+$(function () {
+    // 上に戻るボタンの初期化
+    let topBtn = $('#scrollTop');
+    topBtn.hide();
+
+    // ある程度スクロールされたら、上に戻るボタンを表示する
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            toTop.fadeIn();
+        if ($(this).scrollTop() > 300) {
+            topBtn.fadeIn(); // フェードインで表示
         } else {
-            toTop.fadeOut();
+            topBtn.fadeOut(); // フェードアウトで非表示
         }
     });
 
-    $('.pagetop').click(function (event) {
-        event.preventDefault();
-
+    // クリックで上に戻るボタン
+    topBtn.click(function (event) {
+        event.preventDefault(); // ページ内リンクの挙動をキャンセル
         $('body,html').animate({
             scrollTop: 0
         }, 500);
     });
 });
 
-$(window).on('load', function () {
-    $('body').removeClass('is-slide');
-});
 
-//TOPページ内
-//ふわっと出す
+
+//スクロールした時画像を下からふわっと表示させる方法「フェードイン」
 $(function () {
     $(window).scroll(function () {
-        $('.aboutus_img').each(function () {
+        $('.fadein').each(function () {
             var elemPos = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
@@ -88,286 +63,4 @@ $(function () {
             }
         });
     });
-});
-
-$(function () {
-    $(window).scroll(function () {
-        $('.aboutus_img2').each(function () {
-            var elemPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 200) {
-                $(this).addClass('scrollin');
-            }
-        });
-    });
-});
-
-//ふわっと出す全体
-$(function () {
-    $(window).scroll(function () {
-        $('.fadein2').each(function () {
-            var elemPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 200) {
-                $(this).addClass('scrollin2');
-            }
-        });
-    });
-});
-
-//ハンバーガーメニュー
-$(function () {
-    var $nav = $('#navArea');
-    var $btn = $('.toggle_btn');
-    var $mask = $('#mask');
-    var open2 = 'open2';
-
-    // menu open close
-    $btn.on('click', function () {
-        if (!$nav.hasClass(open2)) {
-            $nav.addClass(open2);
-        } else {
-            $nav.removeClass(open2);
-        }
-    });
-
-    // mask close
-    $mask.on('click', function () {
-        $nav.removeClass(open2);
-    });
-});
-
-
-
-
-
-
-//お買い物ページ合計金額表
-
-$(function () {
-
-    $('input[type="number"]').change(function () {
-
-        // 合計金額の計算
-        $kingaku = 0; // 合計金額
-        $kosuu = 0; // 合計個数
-        $null = true; // 入力有無
-
-        // ボタンの分だけループで一括処理
-        // ボタンの親要素を対象にして金額の合計を計算する
-        $('input[type="number"]').each(function (index, elm) {
-
-            // 計算対象
-            $dl = $(this).parent().parent();
-
-            // 商品名
-            $dt = $dl.find('dt').text();
-            // 金額
-            $label = $dl.find('label').text();
-            // 金額に余計な文字が合ったら消す。今回は”円”
-            $label = $label.replace('円', '');
-
-            // 個数
-            // 数字型に変換しておく
-            $kazu = Number($(this).val());
-
-            // 合計金額に加算、合計個数に加算
-            $kingaku += ($label * $kazu);
-            $kosuu += $kazu;
-
-            //box2 class_change
-            //変更後の文字列
-            $res = $dt + "/" + $(this).val() + "個：" + ($label * $(this).val()) + "円";
-            // 対象指定
-            $target = $('#box2 p:nth-of-type(' + (index + 1) + ')')
-            $target.text($res);
-            if ($(this).val() == 0) {
-                $target.removeClass('open');
-            } else {
-                $target.addClass('open');
-                $null = false;
-                if (($label * $kazu) >= 0) {
-                    $target.css('color', 'Black');
-                } else {
-                    $target.css('color', 'Red');
-                }
-            }
-
-            // カートの上のバッジ
-            $bi = $('.badge i');
-            $bi.attr('data-badge', $kosuu);
-        });
-
-        // カートの上のやつを消すやつ
-        $b = $('.badge');
-        if (!$kosuu) {
-            $b.removeClass('open');
-        } else {
-            $b.addClass('open');
-        }
-
-
-        // テキストをセット
-        $('#total').text("合計：" + $kingaku + "円");
-
-        // 合計金額がマイナスなら赤字
-        if ($kingaku >= 0) {
-            $('#total').css('color', 'Black');
-        } else {
-            $('#total').css('color', 'Red');
-        }
-
-        // box2 class_change
-        if ($null) {
-            $('#box2 p:last-of-type').removeClass('open');
-        } else {
-            $('#box2 p:last-of-type').addClass('open');
-            $res = "合計/" + $kosuu + "個：" + $kingaku + "円";
-            $('#box2 p:last-of-type').text($res);
-        }
-        // #box2に.openがないなら#box2がどっかいく
-        if ($('#box2 .open').length) {
-            $('#box2').addClass('open');
-        } else {
-            $('#box2').removeClass('open');
-        }
-
-
-    });
-
-
-
-
-
-
-
-
-
-    //リセットボタン
-    $('.reset').click(function () {
-        console.log('.reset');
-        // 合計金額の計算
-        $kingaku = 0; // 合計金額
-        $kosuu = 0; // 合計個数
-        $null = true; // 入力有無
-
-        // ボタンの分だけループで一括処理
-        // ボタンの親要素を対象にして金額の合計を計算する
-        $('input[type="number"]').each(function (index, elm) {
-
-            // 計算対象
-            $dl = $(this).parent().parent();
-
-            // 商品名
-            $dt = $dl.find('dt').text();
-            // 金額
-            $label = $dl.find('label').text();
-            // 金額に余計な文字が合ったら消す。今回は”円”
-            $label = $label.replace('円', '');
-
-            // 個数
-            // 数字型に変換しておく
-            $(this).val(0);
-            $kazu = Number($(this).val());
-
-            // 合計金額に加算、合計個数に加算
-            $kingaku += ($label * $kazu);
-            $kosuu += $kazu;
-
-            //box2 class_change
-            //変更後の文字列
-            $res = $dt + "/" + $(this).val() + "個：" + ($label * $(this).val()) + "円";
-            // 対象指定
-            $target = $('#box2 p:nth-of-type(' + (index + 1) + ')')
-            $target.text($res);
-            if ($(this).val() == 0) {
-                $target.removeClass('open');
-            } else {
-                $target.addClass('open');
-                $null = false;
-                if (($label * $kazu) >= 0) {
-                    $target.css('color', 'Black');
-                } else {
-                    $target.css('color', 'Red');
-                }
-            }
-
-            // カートの上のバッジ
-            $bi = $('.badge i');
-            $bi.attr('data-badge', $kosuu);
-        });
-
-        // カートの上のやつを消すやつ
-        $b = $('.badge');
-        if (!$kosuu) {
-            $b.removeClass('open');
-        } else {
-            $b.addClass('open');
-        }
-
-
-        // テキストをセット
-        $('#total').text("合計：" + $kingaku + "円");
-
-        // 合計金額がマイナスなら赤字
-        if ($kingaku >= 0) {
-            $('#total').css('color', 'Black');
-        } else {
-            $('#total').css('color', 'Red');
-        }
-
-        // box2 class_change
-        if ($null) {
-            $('#box2 p:last-of-type').removeClass('open');
-        } else {
-            $('#box2 p:last-of-type').addClass('open');
-            $res = "合計/" + $kosuu + "個：" + $kingaku + "円";
-            $('#box2 p:last-of-type').text($res);
-        }
-        // #box2に.openがないなら#box2がどっかいく
-        if ($('#box2 .open').length) {
-            $('#box2').addClass('open');
-        } else {
-            $('#box2').removeClass('open');
-        }
-
-    });
-
-
-
-
-
-
-
-});
-
-
-$(function () {
-    $('input[type="number"]').change(function () {
-        let controls = $('input[type="number"]'); //$(this)だと1つしか持ってこれない
-        let controlobj = {};
-        let purchaseNumber = 0; //カートアイコンに表示するバッジの数字
-
-        for (let i = 0; i < controls.length; i++) {
-            controlobj[controls[i].name] = $('input[name="' + controls[i].name + '"]').val();
-            purchaseNumber += parseInt($('input[name="' + controls[i].name + '"]').val());
-        }
-        $('#view').load(
-            'php/cart.php',
-            controlobj,
-            function (data, textStatus, XMLHttpRequest) {}
-        );
-
-        $('a.badge').attr('data-badge', purchaseNumber); //カートボタンのバッジの購入数を書き換える
-        $('input.badge').attr('value', purchaseNumber);
-    });
-    $('input[type="reset"]').click(function () {
-        $('#view').empty();
-    });
-
-
-
-
 });
